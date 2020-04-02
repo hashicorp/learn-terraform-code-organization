fd9b1aef1c69b54684b0d929c6c0ac0a7509e5cd
Rachel Sharp <rachel@hashicorp.com>
2020-04-02 11:47:24 -0400

    Terrform Cloud configurations

--- dev/main.tf
+++ dev/main.tf
@@ -2,19 +2,30 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "remote" {
+    hostname      = "app.terraform.io"
+    organization  = "rachel-test"
+
+    workspaces {
+      name = "dev-code-management"
+    }
+  }
+}
+
 module "random_pet" {
-  source = "../modules/random_pet"
+  source = "app.terraform.io/rachel-test/randompet/example"
 }
 
 module "s3" {
   prefix = var.prefix
-  source = "../modules/s3"
+  source = "app.terraform.io/rachel-test/s3/example"
   name   = module.random_pet.name
 }
 
 
 module webapp {
-  source = "../modules/webapp"
+  source = "app.terraform.io/rachel-test/webapp/example"
   bucket = module.s3.bucket 
 }
 
