126437cd55ee722f4a51c71f1130bf20a5058ab4
Rachel Sharp <rachel@hashicorp.com>
2020-04-02 12:45:13 -0400

    new tag 05

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket_object" "webapp" {
   acl          = "public-read"
   key          = "index.html"
   bucket       = var.bucket
-  content      = file("${path.module}/../../assets/index.html")
+  content      = file("${path.module}/assets/index.html")
   content_type = "text/html"
 
 }
