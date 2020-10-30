b205deb0c347eb7d809b868799e5707c880f30e7
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-10-30 11:00:52 -0700

    Update localstack config

---
+++ dev.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = "us-west-2"
+
+  access_key                  = "anaccesskey"
+  secret_key                  = "asecretkey"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+resource "random_pet" "petname" {
+  length    = 4
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "dev" {
+  bucket = "${var.dev_prefix}-${random_pet.petname.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.dev_prefix}-${random_pet.petname.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    # error_document = "error.html"
+  }
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "dev" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.dev.id
+  content      = file("${path.module}/assets/index.html")
+  content_type = "text/html"
+
+}
+
+resource "aws_s3_bucket" "prod" {
+  bucket = "${var.prod_prefix}-${random_pet.petname.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.prod_prefix}-${random_pet.petname.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    # error_document = "error.html"
+  }
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "prod" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.prod.id
+  content      = file("${path.module}/assets/index.html")
+  content_type = "text/html"
+
+}
