a59f9117e573f2d3ee35a2cec2cadb80049d62d5
Robin Norwood <rnorwood@hashicorp.com>
2022-01-25 14:56:37 -0600

    Update bucket policy, provider versions, and fix formatting (workspaces-instruqt branch)

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.73.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
     }
   }
 }
@@ -31,6 +36,17 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "${var.prefix}-${local.bucket_name}"
   acl    = "public-read"
 
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.bucket.id
+
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -43,19 +59,12 @@ resource "aws_s3_bucket" "bucket" {
                 "s3:GetObject"
             ],
             "Resource": [
-                "arn:aws:s3:::${var.prefix}-${local.bucket_name}/*"
+                "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
             ]
         }
     ]
 }
 EOF
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-
-  }
-  force_destroy = true
 }
 
 resource "aws_s3_bucket_object" "webapp" {
